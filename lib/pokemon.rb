ab6cc935469f2f032f0508917b082fc379a96f63
Taylor Donlan <taylordonlan@gmail.com>
2018-06-20 16:34:08 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db, :id, :hp
+
+  def initialize(name:, type:, db:, id:, hp: 60)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+    @hp = hp
+  end
+
+  
 end
